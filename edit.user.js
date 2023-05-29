a164c7a2226852b15306b773b6d36671ba50784b
Marco Bretschneider <meise2000@gmx.net>
2023-05-29 20:31:35 +0200

    Initial form to create pattern

---
+++ edit.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name        Edit Videos from Youtube
+// @namespace   http://rübenkamp.curch
+// @version     0.1
+// @description Helps people to edit videos from youtube with respect to the used pattern
+// @match       https?://www.youtube.com/
+// @grant       none
+// ==/UserScript==
+
+(function() {
+    console.log("called");
+})();
